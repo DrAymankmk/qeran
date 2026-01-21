ea1ad8a5cc29257286b4bcb4cdb560eac750ff16
KareemShaban1 <karim17196@fci.bu.edu.eg>
2026-01-21 12:51:34 +0200

    fix issue in hero section

--- resources/views/frontend/pages/home/partials/hero-section.blade.php
+++ resources/views/frontend/pages/home/partials/hero-section.blade.php
@@ -7,7 +7,9 @@ $heroSection = $homePage->activeSections->where('name', 'hero')->first();
 	<div class="sp-slides">
 
 		@foreach($heroSection->items as $item)
-		<div class="sp-slide her-slider"><img src="{{ $item->images->first()->getUrl() }}" alt="slider"
+		@if($item->images->isNotEmpty() && $item->images->first())
+		<div class="sp-slide her-slider">
+			<img src="{{ $item->images->first()->getUrl() }}" alt="slider"
 				class="sp-image hero-slider-image" />
 			<div class="container">
 				<div class="row">
@@ -34,16 +36,17 @@ $heroSection = $homePage->activeSections->where('name', 'hero')->first();
 							</div>
 						</div>
 						<!-- <div data-width="100%" data-show-transition="left"
-							data-hide-transition="left"
-							data-show-duration="1200" data-show-delay="2000"
-							data-hide-delay="400" class="sp-layer"><a
-								href="services.html"
-								class="main-slider__btn btn btn-default">our
-								features</a></div> -->
+									data-hide-transition="left"
+									data-show-duration="1200" data-show-delay="2000"
+									data-hide-delay="400" class="sp-layer"><a
+										href="services.html"
+										class="main-slider__btn btn btn-default">our
+										features</a></div> -->
 					</div>
 				</div>
 			</div>
 		</div>
+		@endif
 		@endforeach
 
 	</div>

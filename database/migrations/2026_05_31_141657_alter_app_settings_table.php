992e1219df329e58a67f9020e7a9d18595b4b567
KareemShaban1 <karim17196@fci.bu.edu.eg>
2026-05-31 17:21:55 +0300

    update app settings table

---
+++ database/migrations/2026_05_31_141657_alter_app_settings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+		Schema::table('app_settings', function (Blueprint $table) {
+        // change value to longtext
+			$table->longText('value')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+		Schema::table('app_settings', function (Blueprint $table) {
+			$table->string('value')->change();
+		});
+        }
+};

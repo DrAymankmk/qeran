96b35f5648b7a8a02139305b6299864ebaaddf5f
KareemShaban1 <karim17196@fci.bu.edu.eg>
2026-06-12 06:14:53 +0300

    add new updates

--- app/Http/Resources/Invitations/InvitationResource.php
+++ app/Http/Resources/Invitations/InvitationResource.php
@@ -52,6 +52,7 @@ class InvitationResource extends JsonResource
             'extra_invitation_price'=>$this->price,
             'admin_invitation_count'=>$this->admin_invitation_count,
 	   'link_url'=>$this->link_url,
+            'client_preview_url'=>$this->client_preview_url,
             // Hub files split by uploader; recent first in each list
             'media' => $this->formatInvitationMediaByCreator(),
 
